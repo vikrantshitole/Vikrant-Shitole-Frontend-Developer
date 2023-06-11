c6da8c0bdefaa8d81c3e38ad33580aadda8a41d3
Vikrant Shitole <vikrantshitole98@gmail.com>
2023-06-11 16:52:33 +0530

    Header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+export default function Header() {
+  return (
+    <Fragment>    
+    <nav className="navbar navbar-light bg-light">
+      <span className="navbar-brand ml-5 mb-0 h1">Space X</span>
+    </nav>
+    </Fragment>
+   )
+}
