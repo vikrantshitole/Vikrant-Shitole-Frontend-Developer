791ecf2b4d88d0e88d9bc0c392f241c9db0864ac
Vikrant Shitole <vikrantshitole98@gmail.com>
2023-06-11 23:13:31 +0530

    removed unwanted code

--- src/Components/ListView/ListView.js
+++ src/Components/ListView/ListView.js
@@ -25,50 +25,7 @@ export class ListView extends Component {
       <Fragment>
         <div className="row">
           <div className="col-lg-12">
-            <div className="align-items-center row">
-              <div className="col-lg-8">
-                <div className="mb-3 mb-lg-0">
-                  <h6 className="fs-16 mb-0">Showing 1 – 8 of 11 results</h6>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="candidate-list-widgets">
-                  <div className="row">
-                    <div className="col-lg-6">
-                      <div className="selection-widget">
-                        <select
-                          className="form-select"
-                          data-trigger="true"
-                          name="choices-single-filter-orderby"
-                          id="choices-single-filter-orderby"
-                          aria-label="Default select example"
-                        >
-                          <option value="df">Default</option>
-                          <option value="ne">Newest</option>
-                          <option value="od">Oldest</option>
-                          <option value="rd">Random</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="col-lg-6">
-                      <div className="selection-widget mt-2 mt-lg-0">
-                        <select
-                          className="form-select"
-                          data-trigger="true"
-                          name="choices-candidate-page"
-                          id="choices-candidate-page"
-                          aria-label="Default select example"
-                        >
-                          <option value="df">All</option>
-                          <option value="ne">8 per Page</option>
-                          <option value="ne">12 per Page</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+      
             {this.props.list.map((l) => (
               <Capsule item={l} key={l.capsule_serial} />
             ))}
